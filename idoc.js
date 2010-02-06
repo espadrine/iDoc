e751fe6ffc0e2aa7b709a480a722ff1a75f4515e
thaddee tyl <thaddee.tyl@gmail.com>
2010-02-06 11:07:33 +0100

    first commit.

---
+++ idoc.js
@@ -0,0 +1,147 @@
+// (c) 2010 Thaddée Tyl. All rights reserved.
+
+chrome.extension.onRequest.addListener(
+	function( request, sender, sendResponse ) {
+		if(request.edit == true) {
+			if(document.designMode) {
+				document.designMode = "on";
+				sendResponse({ok:true});
+			}
+			else sendResponse({ok:false});
+		}
+		else if(request.edit == false) {
+			if(document.designMode) {
+				document.designMode = "off";
+				sendResponse({ok:true});
+			}
+			else sendResponse({ok:false});
+		}
+		else if(request.edit == "ask") {
+			if(document.designMode == "on")
+				sendResponse({ans:true});
+			else sendResponse({ans:false});
+		}
+		else if(request.edit == "do") {
+			if (request.action == "createlink") {
+				var szURL = prompt("Enter a URL:", "http://");
+				if ((szURL != null) && (szURL != "")) {
+					document.execCommand("CreateLink", false, szURL);
+				}
+			}
+			else if (request.action == "insertimage") {
+				imagePath = prompt('Enter Image URL:', 'http://');
+				if ((imagePath != null) && (imagePath != "")) {
+					document.execCommand('InsertImage', false, imagePath);
+				}
+			}
+			else if (request.action == "createtable") {
+				rowstext = prompt("enter rows");
+				colstext = prompt("enter cols");
+				rows = parseInt(rowstext);
+				cols = parseInt(colstext);
+				if ((rows > 0) && (cols > 0)) {
+				  table = document.createElement("table");
+				  table.setAttribute("border", "1");
+				  table.setAttribute("cellpadding", "2");
+				  table.setAttribute("cellspacing", "2");
+				  tbody = createElement("tbody");
+				  for (var i=0; i < rows; i++) {
+					tr = document.createElement("tr");
+					for (var j=0; j < cols; j++) {
+					  td = document.createElement("td");
+					  br = document.createElement("br");
+					  td.appendChild(br);
+					  tr.appendChild(td);
+					}
+					tbody.appendChild(tr);
+				  }
+				  table.appendChild(tbody);      
+				  insertNodeAtSelection(e.contentWindow, table);
+				}
+			  }
+			else {
+				if(request.arg)
+					document.execCommand(request.action, false, request.arg);
+				else document.execCommand(request.action, false, null);
+			}
+			sendResponse({ok:true});
+
+		}
+		else
+			sendResponse({});
+	});
+
+function insertNodeAtSelection(win, insertNode) {
+  // get current selection
+  var sel = win.getSelection();
+
+  // get the first range of the selection
+  // (there's almost always only one range)
+  var range = sel.getRangeAt(0);
+
+  // deselect everything
+  sel.removeAllRanges();
+
+  // remove content of current selection from document
+  range.deleteContents();
+
+  // get location of current selection
+  var container = range.startContainer;
+  var pos = range.startOffset;
+
+  // make a new range for the new selection
+  range=document.createRange();
+
+  if (container.nodeType==3 && insertNode.nodeType==3) {
+
+	// if we insert text in a textnode, do optimized insertion
+	container.insertData(pos, insertNode.nodeValue);
+
+	// put cursor after inserted text
+	range.setEnd(container, pos+insertNode.length);
+	range.setStart(container, pos+insertNode.length);
+
+  } else {
+
+
+	var afterNode;
+	if (container.nodeType==3) {
+
+	  // when inserting into a textnode
+	  // we create 2 new textnodes
+	  // and put the insertNode in between
+
+	  var textNode = container;
+	  container = textNode.parentNode;
+	  var text = textNode.nodeValue;
+
+	  // text before the split
+	  var textBefore = text.substr(0,pos);
+	  // text after the split
+	  var textAfter = text.substr(pos);
+
+	  var beforeNode = document.createTextNode(textBefore);
+	  afterNode = document.createTextNode(textAfter);
+
+	  // insert the 3 new nodes before the old one
+	  container.insertBefore(afterNode, textNode);
+	  container.insertBefore(insertNode, afterNode);
+	  container.insertBefore(beforeNode, insertNode);
+
+	  // remove the old node
+	  container.removeChild(textNode);
+
+	} else {
+
+	  // else simply insert the node
+	  afterNode = container.childNodes[pos];
+	  container.insertBefore(insertNode, afterNode);
+	}
+
+	range.setEnd(afterNode, 0);
+	range.setStart(afterNode, 0);
+  }
+
+  sel.addRange(range);
+}
+
