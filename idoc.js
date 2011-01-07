e18edfd97f86efee7d8f871609960a35e014b34b
thaddee tyl <thaddee.tyl@gmail.com>
2011-01-07 12:07:19 +0100

    Table bug solved.

--- idoc.js
+++ idoc.js
@@ -6,61 +6,68 @@
 var name = '';
 var procol = document.location.protocol;
 if     (procol == 'http:') name = document.URL.substring(7);
-else if(procol == 'file:') name = document.URL.substring(8);
+else if (procol == 'file:') name = document.URL.substring(8);
 
-// 1. You have to learn to listen!
+/* 1. You have to learn to listen! */
 chrome.extension.onRequest.addListener(
-	function( request, sender, sendResponse ) {
-		if(request.edit == true) {
-			if(document.designMode) {
-				if(request.replace) {
-					document.open();
-					document.write('<!DOCTYPE html>\n<html>'
-								   +request.replace+'</html>');
-					document.close();
-				}
-				if(request.name) {
-					name = request.name;
-					document.title = name;
-				}
-				else if(request.askName) {
-					name = prompt("Name of the file:"); // ask name.
-					if(!name) name = 'ThisIsNotAName'; // frivolous name!
-					document.title = name;
-				}
-				document.designMode = "on";
-				sendResponse({ok:true});
-			}
-			else sendResponse({ok:false});
-		}
-		else if(request.edit == false) {
-			if(document.designMode) {
-				document.designMode = "off";
-				sendResponse({ok:true, save:name,
-				 content: document.getElementsByTagName('html')[0].innerHTML.replace(/idoc\.js/,'')});
-			}
-			else sendResponse({ok:false});
-		}
-		else if(request.edit == "ask") {
-			if(document.designMode == "on") sendResponse({editing:true});
-			else sendResponse({editing:false});
-		}
-		else if(request.edit == "askName") {
-			var range = document.createRange();
-			range.selectNode(document.body);
-			var content = '';
-			var body = document.body.childNodes;
-			for(var i=0; i<body.length; i++)
-				content += body[i].textContent+'\n';
-			sendResponse({name:name, content:content});
-		}
-		else if(request.edit == 'draft') {
-			haveSaved = true;
-			sendResponse({ok:true,save:name,content:document.getElementsByTagName('html')[0].innerHTML.replace(/idoc\.js/,'')}); // avoids infinite loop
-		}
-    else if(request.edit == "css") {
+  function( request, sender, sendResponse ) {
+    if (request.edit == true) {
+      if (document.designMode) {
+        if (request.replace) {
+          document.open();
+          document.write('<!DOCTYPE html>\n<html>'
+                         +request.replace+'</html>');
+          document.close();
+        }
+        if (request.name) {
+          if (request.name != true) { name = request.name; }
+        } else {
+          name = prompt("Name of the file:"); // ask name.
+          if (!name) { name = 'ThisIsNotAName'; } // frivolous name!
+        }
+        document.title = name;
+        document.designMode = "on";
+        sendResponse({ok:true});
+      }
+      else { sendResponse({ok:false}); }
+    } else if (request.edit == false) {
+      if (document.designMode) {
+        document.designMode = "off";
+        sendResponse({ok:true, save:name,
+         content: document.getElementsByTagName('html')[0].innerHTML.replace(/idoc\.js/,'')
+        });
+      }
+      else sendResponse({ok:false});
+      
+    } else if (request.edit == "ask") {
+      if (document.designMode == "on") { sendResponse({editing:true}); }
+      else { sendResponse({editing:false}); }
+
+    } else if (request.edit == "askName") {
+      var range = document.createRange();
+      range.selectNode(document.body);
+      var content = '';
+      var body = document.body.childNodes;
+      for(var i=0; i<body.length; i++) {
+        content += body[i].textContent+'\n';
+      }
+      sendResponse({name:name, content:content});
+
+    } else if (request.edit == 'draft') {
+      haveSaved = true;
+      sendResponse({
+          ok:true,
+          save:name,
+          content: (function() {
+            var doc = document.getElementsByTagName('html')[0].innerHTML;
+            doc.replace(/idoc\.js/,''); /* Avoids infinite loops. */
+            return doc;
+          })()
+      });
+
+    } else if (request.edit == "css") {
       var csstext = '';
-      if(!document.getElementById('idocCSS')) {
+      if (!document.getElementById('idocCSS')) {
         /* create style element */
         document.body.insertAdjacentHTML('afterbegin',
             '<style id="idocCSS"></style>');
@@ -68,13 +75,15 @@ chrome.extension.onRequest.addListener(
         csstext = document.getElementById('idocCSS').innerHTML;
       }
       /* show edit box */
-      if(!document.getElementById('idocCSSbox')) {
+      if (!document.getElementById('idocCSSbox')) {
         /* create box */
         document.body.insertAdjacentHTML('beforeend',
           '<div id="idocCSSbox" style="position:fixed;top:20px;left:20px;text-align:center;background-color:#f3f7fc;border:solid 1px #d2dbed;border-radius:3px;padding:10px;display:block;">'+
           '<textarea id="idocCSStext" rows=10 style="font-family:monospace;"></textarea><br>'+
           '<button id="idocCSSdone">done</button></div>');
       } else {
+        document.getElementById('idocCSStext').style.height =
+            (window.innerHeight - 60) + 'px';
         document.getElementById('idocCSSbox').style.display = 'block';
         document.getElementById('idocCSSbox').firstChild.value=csstext;
       }
@@ -87,132 +96,127 @@ chrome.extension.onRequest.addListener(
           document.getElementById('idocCSSbox').style.display = 'none';
         },false);
       sendResponse({});
-    }
-		else if(request.edit == "do") {
-			if (request.action == "createlink") {
-				var szURL = prompt("Enter a URL:", "http://");
-				if ((szURL != null) && (szURL != "")) {
-					document.execCommand("CreateLink", false, szURL);
-				}
-			}
-			else if (request.action == "insertimage") {
-				imagePath = prompt('Enter Image URL:', 'http://');
-				if ((imagePath != null) && (imagePath != "")) {
-					document.execCommand('InsertImage', false, imagePath);
-				}
-			}
-			else if (request.action == "createtable") {
-				rowstext = prompt("enter rows");
-				colstext = prompt("enter cols");
-				rows = parseInt(rowstext);
-				cols = parseInt(colstext);
-				if ((rows > 0) && (cols > 0)) {
-				  table = document.createElement("table");
-				  table.setAttribute("border", "1");
-				  table.setAttribute("cellpadding", "2");
-				  table.setAttribute("cellspacing", "2");
-				  tbody = createElement("tbody");
-				  for (var i=0; i < rows; i++) {
-					tr = document.createElement("tr");
-					for (var j=0; j < cols; j++) {
-					  td = document.createElement("td");
-					  br = document.createElement("br");
-					  td.appendChild(br);
-					  tr.appendChild(td);
-					}
-					tbody.appendChild(tr);
-				  }
-				  table.appendChild(tbody);      
-				  insertNodeAtSelection(e.contentWindow, table);
-				}
-			  }
-			else {
-				if(request.arg)
-					document.execCommand(request.action, false, request.arg);
-				else document.execCommand(request.action, false, null);
-			}
-			sendResponse({ok:true});
+      
+    } else if (request.edit == "do") {
+      if (request.action == "createlink") {
+        var szURL = prompt("Enter a URL:", "http://");
+        if ((szURL != null) && (szURL != "")) {
+          document.execCommand("CreateLink", false, szURL);
+        }
+      } else if (request.action == "insertimage") {
+        imagePath = prompt('Enter Image URL:', 'http://');
+        if ((imagePath != null) && (imagePath != "")) {
+          document.execCommand('InsertImage', false, imagePath);
+        }
+      } else if (request.action == "createtable") {
+        rowstext = prompt("enter rows");
+        colstext = prompt("enter cols");
+        rows = parseInt(rowstext);
+        cols = parseInt(colstext);
+        if ((rows > 0) && (cols > 0)) {
+          table = document.createElement("table");
+          table.setAttribute("border", "1");
+          table.setAttribute("cellpadding", "2");
+          table.setAttribute("cellspacing", "2");
+          tbody = document.createElement("tbody");
+          for (var i=0; i < rows; i++) {
+          tr = document.createElement("tr");
+          for (var j=0; j < cols; j++) {
+            td = document.createElement("td");
+            br = document.createElement("br");
+            td.appendChild(br);
+            tr.appendChild(td);
+          }
+          tbody.appendChild(tr);
+          }
+          table.appendChild(tbody);      
+          insertNodeAtSelection(window, table);
+        }
+      } else {
+        if (request.arg)
+          document.execCommand(request.action, false, request.arg);
+        else document.execCommand(request.action, false, null);
+      }
+      sendResponse({ok:true});
 
-		}
-		else
-			sendResponse({});
-	}
+    } else { sendResponse({}); }
+  }
 );
 
-// 2. Wiring things up inside the DOM forest.
+/* 2. Wiring things up inside the DOM forest. */
 function insertNodeAtSelection(win, insertNode) {
-  var sel = win.getSelection(); // get current selection
+  var sel = win.getSelection(); // Get current selection
 
-  // get the first range of the selection
+  // Get the first range of the selection
   // (there's almost always only one range)
   var range = sel.getRangeAt(0);
 
-  sel.removeAllRanges(); // deselect everything
+  sel.removeAllRanges(); // Deselect everything
 
-  range.deleteContents();  // remove content of current selection from document
+  range.deleteContents();  // Remove content of current selection from document
 
-  var container = range.startContainer;  // get location of current selection
+  var container = range.startContainer;  // Get location of current selection
   var pos = range.startOffset;
 
-  range = document.createRange();  // make a new range for the new selection
+  range = document.createRange();  // Make a new range for the new selection
 
-  if(container.nodeType==3 && insertNode.nodeType==3) {
-	// if we insert text in a textnode, do optimized insertion
-	container.insertData(pos, insertNode.nodeValue);
+  if (container.nodeType==3 && insertNode.nodeType==3) {
+    // If we insert text in a textnode, do optimized insertion
+    container.insertData(pos, insertNode.nodeValue);
 
-	// put cursor after inserted text
-	range.setEnd(container, pos+insertNode.length);
-	range.setStart(container, pos+insertNode.length);
+    // Put cursor after inserted text
+    range.setEnd(container, pos+insertNode.length);
+    range.setStart(container, pos+insertNode.length);
   }
   else {
-	var afterNode;
-	if(container.nodeType==3) {
-	  // when inserting into a textnode
-	  // we create 2 new textnodes
-	  // and put the insertNode in between
+    var afterNode;
+    if (container.nodeType==3) {
+      // When inserting into a textnode
+      // We create 2 new textnodes
+      // and put the insertNode in between
 
-	  var textNode = container;
-	  container = textNode.parentNode;
-	  var text = textNode.nodeValue;
+      var textNode = container;
+      container = textNode.parentNode;
+      var text = textNode.nodeValue;
 
-	  var textBefore = text.substr(0,pos);	  // text before the split
-	  var textAfter = text.substr(pos);	  // text after the split
+      var textBefore = text.substr(0,pos);    // text before the split
+      var textAfter = text.substr(pos);    // text after the split
 
-	  var beforeNode = document.createTextNode(textBefore);
-	  afterNode = document.createTextNode(textAfter);
+      var beforeNode = document.createTextNode(textBefore);
+      afterNode = document.createTextNode(textAfter);
 
-	  // insert the 3 new nodes before the old one
-	  container.insertBefore(afterNode, textNode);
-	  container.insertBefore(insertNode, afterNode);
-	  container.insertBefore(beforeNode, insertNode);
+      // insert the 3 new nodes before the old one
+      container.insertBefore(afterNode, textNode);
+      container.insertBefore(insertNode, afterNode);
+      container.insertBefore(beforeNode, insertNode);
 
-	  container.removeChild(textNode);	  // remove the old node
-	}
-	else {
-	  // else simply insert the node
-	  afterNode = container.childNodes[pos];
-	  container.insertBefore(insertNode, afterNode);
-	}
+      container.removeChild(textNode);    // remove the old node
+    }
+    else {
+      // else simply insert the node
+      afterNode = container.childNodes[pos];
+      container.insertBefore(insertNode, afterNode);
+    }
 
-	range.setEnd(afterNode, 0);
-	range.setStart(afterNode, 0);
+    range.setEnd(afterNode, 0);
+    range.setStart(afterNode, 0);
   }
 
   sel.addRange(range);
 }
 
-// 3. Do you want to lose your edition?
+/* 3. Do you want to lose your edition? */
 var haveSaved = true;
 window.onbeforeunload = function(e) {
-	if(document.designMode=='on' && !haveSaved) { //FIXME
-		// save it, just in case :)
-		chrome.extension.sendRequest({save: name,
-				content: document.getElementsByTagName('html')[0].innerHTML.replace(/idoc\.js/,'')},
-				function(r){});
-	}
+  if (document.designMode=='on' && !haveSaved) { //FIXME
+    // save it, just in case :)
+    chrome.extension.sendRequest({save: name,
+        content: document.getElementsByTagName('html')[0].innerHTML.replace(/idoc\.js/,'')},
+        function(r){});
+  }
 }
 document.onkeydown = function(e) {
-	if(e.keyCode == 83 && (e.ctrlKey||e.metaKey)) // CTRL/Command+S
-		haveSaved = true;
-	else haveSaved = false;
+  if (e.keyCode == 83 && (e.ctrlKey||e.metaKey)) { // CTRL/Command+S
+    haveSaved = true;
+  } else { haveSaved = false; }
 }
